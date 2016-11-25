7ca4712a548cfe83661a3f66bcaaa41e67077288
Kreest <kreest503@gmail.com>
2016-11-25 23:36:03 +0100

    WIP

--- 2015-3f3/egyenletes/egyenletes.py
+++ 2015-3f3/egyenletes/egyenletes.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 M, d = map(int, input().split())
 entries = [int(x) for x in input().split()]
-magicnumber = sum(entries) // d
+magic = sum(entries) // d
+print(magic)
 rem = sum(entries) % d
 if d == 1:
 	print(*[item for sublist in [[x+1] * y for x, y in enumerate(entries)] for item in sublist])
 elif d == 2:
-	pass
+	m = [[0 for _ in range(0, magic)] for _ in range(0, M)]
+
+	for i in range(1, M):
+		for j in range(0, magic):
+			if entries[i] > j:
+				m[i][j] = m[i-1][j]
+			else:
+				m[i][j] = max(m[i-1][j], m[i-1][j-entries[i]]+i)
+	print(m)
 elif d == 3:
 	pass
