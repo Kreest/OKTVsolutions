17c4fd43a259cb10512cd23ccf9936b0e59598b2
Kreest <kreest503@gmail.com>
2016-12-10 21:16:53 +0100

    Cleaned up the solution

--- 2016-3f3/epulet/epulet.py
+++ 2016-3f3/epulet/epulet.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-from math import ceil
 N, I = map(int, input().split())
 size = 3 * 2 ** (N - 1)
-if size < I:
-	print("error")
 res = ""
 helper = {(3, 1): ('F', 0), (3, 2): ('P', 0), (3, 3): ('Z', 0), (6, 1): ('FP', 0), (6, 2): ('PF', 0), (6, 3): ('PP', 0), (6, 4): ('PZ', 0), (6, 5): ('ZF', 0), (6, 6): ('ZZ', 0)}
 while len(res) < N:
@@ -15,13 +12,13 @@ while len(res) < N:
 			res += "FP"
 		elif I <= size * 3 / 4:
 			I -= size // 4
-			size = size // 2
+			size //= 2
 			res += "P"
 		else:
 			I -= size * 3 // 4
 			size = size // 4
 			res += "Z"
-			while I > size / 2 :
+			while len(res) < N and I > size / 2:
 				res+="Z"
 				I -= size / 2
 				size /= 2
--- 2016-3f3/epulet/test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for %%f in (*.*) do (
-	if "%%~xf" == ".txt" (
-		type %%f
-		echo : 
-		py epulet.py < %%f))
