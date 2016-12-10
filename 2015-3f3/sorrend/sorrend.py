10a5aec8e7d7d7b53c8d84d08ab22fa8c4190557
Kreest <kreest503@gmail.com>
2016-12-10 21:02:40 +0100

    Fixed an issue, discovered lru_cache

--- 2015-3f3/sorrend/sorrend.py
+++ 2015-3f3/sorrend/sorrend.py
@@ -1,13 +1,12 @@
-﻿import sys
-sys.setrecursionlimit(50000)
-memo = {} #Memoization: storing the functions calculated output for a certain input, then on next call just returning it from memory. Vastly improves recursion speed. More fun that rewriting iterativ
+﻿#!/usr/bin/env python3
+memo = {} #Memoization is storing the functions calculated output for a certain input, then on next call just returning it from memory. Vastly improves recursion speed. More fun that rewriting iteratively
 
 def perm(n):
     if n <= 2:
         return 1
     if n in memo:
         return memo[n]
-    memo[n] = (n-1) * perm(n - 1) + (n - 2) * perm(n - 2) #The complicated part is deriving this formula, explained in formula.txt
+    memo[n] = (n-1) * perm(n-1) + (n-2) * perm(n-2) #The complicated part is deriving this formula, explained in formula.txt
     return memo[n]
 
-print(aiter(50))
+print(perm(50))
