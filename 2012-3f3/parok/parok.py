0a038335596485e7eee57c7a913e241175ddb859
Kreest <kreest503@gmail.com>
2017-05-01 15:32:11 +0200

    More cleanup

--- 2012-3f3/parok/parok.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from collections import defaultdict
-import bisect
-pairs = defaultdict(lambda: [])
-N, M = map(int, input().split())
-for line in sys.stdin.readlines():
-	a, b = map(int, line.split())
-	bisect.insort(pairs[a], b)
